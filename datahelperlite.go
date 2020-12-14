1778c0a9fdee9bcdff0f334f5a97de8468e79fd5
Elizalde Baguinon <mizmerize@yahoo.com>
2020-12-14 19:46:49 +0800

    Initial commit

---
+++ datahelperlite.go
@@ -0,0 +1,85 @@
+package datahelperlite
+
+import (
+	"context"
+	"errors"
+	"regexp"
+
+	cfg "github.com/eaglebush/config"
+	std "github.com/eaglebush/stdutil"
+)
+
+// DataHelperLite interface for usage
+type DataHelperLite interface {
+	Open(ctx context.Context, di *cfg.DatabaseInfo) error
+	Close() error
+	Begin() error
+	Commit() error
+	Rollback() error
+	Mark(name string) error
+	Discard(name string) error
+	Save(name string) error
+	Query(sql string, args ...interface{}) (Rows, error)
+	QueryRow(sql string, args ...interface{}) Row
+	Exec(sql string, args ...interface{}) (int64, error)
+	VerifyWithin(tablename string, values []std.ValidationExpression) (Valid bool, QueryOK bool, Message string)
+}
+
+// Helper for datahelperlite
+var Helper map[string]DataHelperLite
+
+// New creates new datahelper lite
+func New(dhl *DataHelperLite, helperid string) (DataHelperLite, error) {
+
+	var (
+		ndh     DataHelperLite
+		present bool
+	)
+	// copy existing postgresql helper
+	ndh = *dhl
+
+	if ndh == nil {
+		ndh, present = Helper[helperid]
+		if !present {
+			return nil, errors.New(`No helper name of such`)
+		}
+	}
+
+	return ndh, nil
+}
+
+// SetHelper - set helper object
+func SetHelper(name string, dhl DataHelperLite) {
+	if Helper == nil {
+		Helper = make(map[string]DataHelperLite)
+	}
+
+	Helper[name] = dhl
+}
+
+// Row datahelperlite row interface
+type Row interface {
+	Scan(dest ...interface{}) error
+}
+
+// Rows datahelperlite rows interface
+type Rows interface {
+	Close()
+	Err() error
+	Next() bool
+	Scan(dest ...interface{}) error
+	Values() ([]interface{}, error)
+	RawValues() [][]byte
+}
+
+// InterpolateTable interpolates table name that has been enclosed with curly braces
+func InterpolateTable(sql string, schema string) string {
+	if schema != "" {
+		schema = schema + `.`
+	}
+
+	re := regexp.MustCompile(`\{([a-zA-Z0-9\[\]\"\_\-]*)\}`)
+	sql = re.ReplaceAllString(sql, schema+`$1`)
+
+	return sql
+}
