18d3aaf2e9f695e1bf65a2e8248392dfac77d343
Elizalde Baguinon <mizmerize@yahoo.com>
2022-01-06 20:03:40 +0800

    Update rows.go

--- rows.go
+++ rows.go
@@ -1,14 +1,12 @@
 package datahelperlite
 
-import "database/sql"
-
 // Rows datahelperlite rows interface
 type Rows interface {
 	Close()
 	Err() error
 	Next() bool
 	Scan(dest ...interface{}) error
-	Columns() ([]sql.ColumnType, error)
+	Columns() ([]Column, error)
 	Values() ([]interface{}, error)
 	RawValues() [][]byte
 }
