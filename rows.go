33c74727e401aeac4bf6f6c84012095c6d2876e3
Elizalde Baguinon <mizmerize@yahoo.com>
2024-09-17 17:56:08 +0800

    Added Paged

--- rows.go
+++ rows.go
@@ -2,11 +2,13 @@ package datahelperlite
 
 // Rows datahelperlite rows interface
 type Rows interface {
-	Close()
-	Err() error
-	Next() bool
-	Scan(dest ...interface{}) error
-	Columns() ([]Column, error)
-	Values() ([]interface{}, error)
-	RawValues() [][]byte
+	Close()                         // Close the rows
+	Columns() ([]Column, error)     // Get columns
+	Err() error                     // Get last error
+	Next() bool                     // Get next number as specified in the upsert configuration
+	PageCount() int                 // The total number of pages as the result of page size configuration and the result
+	PageID() string                 // The page id returned when the rows were retrieved via Pager
+	RawValues() [][]byte            // Raw values in array
+	Scan(dest ...interface{}) error // Put the result into destination variables
+	Values() ([]interface{}, error) // Return the values
 }
