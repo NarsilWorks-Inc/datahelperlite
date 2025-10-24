3aae3a9d71b4a479f7a5af7a0266b328bdf0b9a5
Elizalde Baguinon <mizmerize@yahoo.com>
2025-10-24 09:06:50 +0800

    Update data-helper-handle.go

--- v3/data-helper-handle.go
+++ v3/data-helper-handle.go
@@ -1,11 +1,8 @@
 package datahelperlite
 
 import (
-	"context"
 	"database/sql"
 	"errors"
-	"fmt"
-	"time"
 
 	dn "github.com/eaglebush/datainfo"
 )
@@ -13,87 +10,18 @@ import (
 // DataHelperHandle manages the handle to the database connection
 //
 // It manages the resident database connection for proper pooling
-type DataHelperHandle struct {
-	db  *sql.DB
-	dbi *dn.DataInfo
-	err error
+type DataHelperHandle interface {
+	Open(di *dn.DataInfo) error
+	Ping() error
+	DB() *sql.DB
+	DI() *dn.DataInfo
+	Close() error
+	Err() error
 }
 
 // Errors
 var (
-	ErrNoConn    error = errors.New(`no connection of the object was initialized`)
-	ErrNoConnStr error = errors.New(`connection string not set`)
-	ErrNoHandle  error = errors.New(`no sql handle`)
+	ErrHandleNoConn    error = errors.New(`no connection of the object was initialized`)
+	ErrHandleNoConnStr error = errors.New(`connection string not set`)
+	ErrHandleNoHandle  error = errors.New(`no sql handle`)
 )
-
-// Open connects to the database and initializes it
-func (h *DataHelperHandle) Open(di *dn.DataInfo) error {
-	if di == nil {
-		return fmt.Errorf("open: no data info set")
-	}
-	if di.ConnectionString == nil {
-		return fmt.Errorf("open: no data connection string set")
-	}
-	h.db, h.err = sql.Open("sqlserver", *di.ConnectionString)
-	if h.err != nil {
-		return fmt.Errorf("open: %w", h.err)
-	}
-	h.dbi = di
-	if di.MaxOpenConnection != nil {
-		h.db.SetMaxOpenConns(*di.MaxOpenConnection)
-	}
-	h.db.SetMaxIdleConns(0)
-	if di.MaxIdleConnection != nil {
-		h.db.SetMaxIdleConns(*di.MaxIdleConnection)
-	}
-	if di.MaxConnectionLifetime != nil {
-		h.db.SetConnMaxLifetime(time.Duration(*di.MaxConnectionLifetime))
-	}
-	if di.MaxConnectionIdleTime != nil {
-		h.db.SetConnMaxIdleTime(time.Duration(*di.MaxConnectionIdleTime))
-	}
-	if err := h.db.PingContext(context.Background()); err != nil {
-		h.err = fmt.Errorf("open: %w", err)
-		return h.err
-	}
-	return nil
-}
-
-// Ping tests the database connection
-func (h *DataHelperHandle) Ping() error {
-	if h.db == nil {
-		return fmt.Errorf("ping: %s to use", ErrNoHandle)
-	}
-	if err := h.db.PingContext(context.Background()); err != nil {
-		h.err = fmt.Errorf("ping: %w", err)
-		return h.err
-	}
-	return nil
-}
-
-// DB returns the database handle
-func (h *DataHelperHandle) DB() *sql.DB {
-	return h.db
-}
-
-// DI returns the data info that configured the handle
-func (h *DataHelperHandle) DI() *dn.DataInfo {
-	return h.dbi
-}
-
-// Close the database connection
-func (h *DataHelperHandle) Close() error {
-	if h.db == nil {
-		return fmt.Errorf("ping: %s to close", ErrNoHandle)
-	}
-	if h.err = h.db.Close(); h.err != nil {
-		return h.err
-	}
-	h.db = nil
-	return nil
-}
-
-// Err returns the last error
-func (h *DataHelperHandle) Err() error {
-	return h.err
-}
